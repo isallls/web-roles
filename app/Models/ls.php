dc953328ab171937f5e28697942861aa999f7986
isallls <faisalsdr306@gmail.com>
2024-03-25 11:56:06 +0700

    'v1'

--- app/Models/ls.php
+++ app/Models/ls.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ls extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'mmm'
-    ];
+    // protected $fillable = [
+    //     'mmm'
+    // ];
 }
